4407f3474c628f45d1f4bf4d0f2d3590716d0927
821N <nathaniel.marofsky@gmail.com>
2020-01-22 19:43:52 -0500

    fix string

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -18,7 +18,7 @@ def root():
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ test """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
